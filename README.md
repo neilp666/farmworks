f3e4da517a5c847d7563c9c85118620914f4d877
neil patel <neil@myindianweddingapp.com>
2014-05-19 19:50:45 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+== README
+
+Farmworks demo application
+
+* Ruby 2.0
+
+* Rails 4.0.4
+
+* Bootstrap 3.0
+
+* Prodution - Postgres
+
+* Development - SQL
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Farmworks::Application.config.secret_key_base = 'e5e9b87ca5dad4018a7a12bcd848ee52b0bd5770f0e504981ef2362a785a1cb72aa6936b0fefcaeae115fe4de15efda2bbb5d43ac775a6c6382eb6350764e440'
