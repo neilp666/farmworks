cc693738e847b03850a5c80e6d576f4bdff08140
neil patel <neil@myindianweddingapp.com>
2014-05-20 20:01:36 +0100

    Added custom CSS

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,2 +1,5 @@
 class Listing < ActiveRecord::Base
+
+	has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
+	validates_attachment_content_type :image, content_type: %w(image/jpeg image/jpg image/png)
 end
