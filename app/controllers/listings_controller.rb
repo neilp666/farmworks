b3f8ccf165698a28fcb0f54036d451cf5db09330
neil patel <neil@myindianweddingapp.com>
2014-05-21 14:29:25 +0100

    Add Seller Page

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,12 +1,17 @@
 class ListingsController < ApplicationController
   before_action :set_listing, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:seller, :new, :create, :edit, :update, :destroy]
   before_action :check_user, only: [:edit, :update, :destroy]
 
   # GET /listings
   # GET /listings.json
+
+  def seller
+    @listings = Listing.where(user: current_user).order("created_at DESC")
+  end
+
   def index
-    @listings = Listing.all
+    @listings = Listing.all.order("created_at DESC")
   end
 
   # GET /listings/1
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Farmworks::Application.routes.draw do
 
   get "about" => 'pages#about'
   get "contact" => 'pages#contact'
+  get 'seller' => "listings#seller"
 
   root 'listings#index'
   # The priority is based upon order of creation: first created -> highest priority.
